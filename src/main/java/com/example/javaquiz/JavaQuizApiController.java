97f8446e52a5db0b43d57dd9c6c7422777d0380f
hibikon <hibikon.asr.mac@gmail.com>
2021-03-31 12:16:29 +0900

    create html for quiz

--- src/main/java/com/example/javaquiz/JavaQuizApiController.java
+++ src/main/java/com/example/javaquiz/JavaQuizApiController.java
@@ -1,21 +1,17 @@
 package com.example.javaquiz;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-@Controller
-@RequestMapping("page")
-public class JavaQuizController {
+@RestController
+public class JavaQuizApiController {
 
     // Quizクラス
     private List<Quiz> quizzes = new ArrayList<>();
@@ -24,36 +20,30 @@ public class JavaQuizController {
 
 
     @GetMapping("/quiz")
-    public String quiz(Model model) {
+    public Quiz quiz() {
         int index = new Random().nextInt(quizzes.size());
-        model.addAttribute("quiz", quizzes.get(index));
-
-        return "quiz";
+        return quizzes.get(index);
     }
 
 
     // showメソッド
     @GetMapping("/show")
-    public String show(Model model) {
-        model.addAttribute("quizzes", quizzes);
-
-        return "list";
+    public List<Quiz> show() {
+        return quizzes;
     }
 
 
     // createメソッド
     @PostMapping("/create")
-    public String create(@RequestParam String question, @RequestParam boolean answer) {
+    public void create(@RequestParam String question, @RequestParam boolean answer) {
         Quiz quiz = new Quiz(question, answer);
         quizzes.add(quiz);
-
-        return "redirect:/page/show";
     }
 
 
     // checkメソッド
     @GetMapping("/check")
-    public String check(Model model, @RequestParam String question, @RequestParam boolean answer) {
+    public String check(@RequestParam String question, @RequestParam boolean answer) {
 
         // 解答が正しいかどうかをチェックして、結果を返す
         // 指定されたquestionを登録済みのクイズから検索
@@ -61,52 +51,47 @@ public class JavaQuizController {
 
             // クイズが見つかった場合...
             if (quiz.getQuestion().equals(question)) {
-                model.addAttribute("quiz", quiz);
 
                 // 登録されているanswerと解答として送信されたanswerが一致
                 if (quiz.isAnswer() == answer) {
-                    model.addAttribute("result","正解!");
-
+                    return "正解！";
                 }
                 // 登録されているanswerと解答として送信されたanswerが不一致
                 else {
-                    model.addAttribute("result","不正解!");
+                    return "不正解！";
                 }
             }
         }
 
-        return "answer";
+        // クイズが見つからなかった場合...
+        return "見つからなかった!";
     }
 
 
     // saveメソッド
     @PostMapping("/save")
-    public String save(RedirectAttributes attributes) {
+    public String save() {
         try {
             javaQuizFileDao.write(quizzes);
-            attributes.addFlashAttribute("successMessage","ファイルに保存しました");
+            return "ファイルに保存しました";
         }
         catch (IOException e) {
             e.printStackTrace();
-            attributes.addFlashAttribute("errorMessage","ファイルの保存に失敗しました");
+            return " ファイルへの保存に失敗しました";
         }
-
-        return "redirect:/page/show";
     }
 
 
     // loadメソッド
     @GetMapping("/load")
-    public String load(RedirectAttributes attributes) {
+    public String load() {
         try {
             quizzes = javaQuizFileDao.read();
-            attributes.addFlashAttribute("successMessage","ファイルを読み込みました");
+            return "ファイルを読み込みました";
         }
         catch (IOException e) {
             e.printStackTrace();
-            attributes.addFlashAttribute("errorMessage","ファイルの読み込みに失敗しました");
+            return "ファイルの読み込みに失敗しました";
         }
-
-        return "redirect:/page/show";
     }
 }
